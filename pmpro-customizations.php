66738b1bb1a6fdeb1838b6fd2cb24cde3a1ca78a
Tomasz Finc <tomasz@gmail.com>
2019-03-18 22:34:38 -0700

    Adding new email frequency settings

--- pmpro-customizations.php
+++ pmpro-customizations.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: PMPro Customizations by Nicholas
 Plugin URI: https://www.paidmembershipspro.com/wp/pmpro-customizations/
-Description: Customizations for my PMPro for DIA
-Version: .1
-Author: Nicholas
+Description: Customizations for DIA PMPro
+Version: .2
+Author(s): Nicholas, Tomasz
 Author URI: https://www.tion.solutions
 */
  
@@ -28,3 +28,22 @@ function my_pmpro_pages_shortcode_checkout($content)
 	return $temp_content;
 }
 add_filter("pmpro_pages_shortcode_checkout", "my_pmpro_pages_shortcode_checkout");
+
+/**
+* Filter the settings of email frequency sent when using the Extra Expiration Warning Emails Add On
+* https://www.paidmembershipspro.com/add-ons/extra-expiration-warning-emails-add-on/
+*
+* Update the $settings array to your list of number of days => ''.
+* Read the Add On documentation for additional customization using this filter.
+*/
+function custom_pmproeewe_email_frequency( $settings = array() )
+{
+	$settings = array(
+		0 => '',
+		14 => '',
+		30 => '',
+	);
+	return $settings;
+}
+add_filter( 'pmproeewe_email_frequency_and_templates', 'custom_pmproeewe_email_frequency', 10, 1 );
+
